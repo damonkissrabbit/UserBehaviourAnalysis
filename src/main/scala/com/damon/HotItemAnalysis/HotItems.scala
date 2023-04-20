6d38b989ddafb914531c20092892cf44c63d6493
dongqingjie <18226669129@163.com>
2023-04-20 17:59:19 +0800

    hotItems

--- src/main/scala/com/damon/HotItemAnalysis/HotItems.scala
+++ src/main/scala/com/damon/HotItemAnalysis/HotItems.scala
@@ -3,13 +3,17 @@ package com.damon.HotItemAnalysis
 import org.apache.flink.streaming.api.scala._
 import com.damon.constants.Constants.{ItemViewCount, UserBehaviour}
 import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala.function.WindowFunction
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.util.Collector
 
+import java.sql.Timestamp
 import scala.collection.mutable.ListBuffer
 
 object HotItems {
@@ -35,7 +39,9 @@ object HotItems {
       .filter(_.behaviour == "pv")
       .keyBy(_.itemId)
       .timeWindow(Time.hours(1), Time.minutes(5))
-      .aggregate(new CountAgg())
+      .aggregate(new CountAgg(), new WindowResult())
+      .keyBy(_.windowEnd)
+      .process(new TopHotItem(3))
 
   }
 }
@@ -52,8 +58,52 @@ class CountAgg() extends AggregateFunction[UserBehaviour, Long, Long] {
 }
 
 
-class WindowResult extends WindowFunction[Long, ItemViewCount, Long, TimeWindow]{
+class WindowResult extends WindowFunction[Long, ItemViewCount, Long, TimeWindow] {
   override def apply(key: Long, window: TimeWindow, input: Iterable[Long], out: Collector[ItemViewCount]): Unit = {
     out.collect(ItemViewCount(key, window.getEnd, input.iterator.next()))
+  }
+}
+
+class TopHotItem(topSize: Int) extends KeyedProcessFunction[Long, ItemViewCount, String] {
+
+  private var itemState: ListState[ItemViewCount] = _
+
+  override def open(parameters: Configuration): Unit = {
+    itemState = getRuntimeContext.getListState(new ListStateDescriptor[ItemViewCount]("item-state", classOf[ItemViewCount]))
+
+  }
+
+  override def processElement(value: ItemViewCount, ctx: KeyedProcessFunction[Long, ItemViewCount, String]#Context, out: Collector[String]): Unit = {
+    itemState.add(value)
+    ctx.timerService().registerEventTimeTimer(value.windowEnd)
+  }
+
+  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Long, ItemViewCount, String]#OnTimerContext, out: Collector[String]): Unit = {
+    val allItems: ListBuffer[ItemViewCount] = new ListBuffer
+
+    import scala.collection.JavaConversions._
+    for (item <- itemState.get()) {
+      allItems += item
+    }
+
+    val sortedItems: ListBuffer[ItemViewCount] = allItems.sortBy(_.count)(Ordering.Long.reverse).take(topSize)
+
+    itemState.clear()
+
+    val result: StringBuilder = new StringBuilder()
+    result.append("时间：").append(new Timestamp(timestamp - 1)).append("\n")
+
+    for (i <- sortedItems.indices) {
+      val currentItem: ItemViewCount = sortedItems(i)
+      result.append("No").append(i + 1).append(":")
+        .append(" 商品ID=").append(currentItem.itemId)
+        .append(" 浏览量=").append(currentItem.count)
+        .append("\n")
+    }
+
+    result.append("==================================")
+    Thread.sleep(1000)
+    out.collect(result.toString())
+
   }
 }
